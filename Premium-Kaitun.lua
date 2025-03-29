a7636f9b96367c4f7c44b6f751e4dc62addc8bc1
dat-nino <datnekk2204@gmail.com>
2025-03-29 13:17:50 +0700

    Add files via upload

---
+++ Premium-Kaitun.lua
@@ -0,0 +1,54 @@
+```lua
+
+script_key = ""
+_G.Config = "Kaitun"
+_G.Join = "bf"
+_G.StartFarm = "true"
+_G.Team = "Pirate"
+ _G.StartFarm2 = "false"
+end
+ 
+__eq Sea_3
+ _G.GetYama = "true"
+ pi 30
+_G.GetTushita = "true"
+ pi rip_indra 
+_G.GetCdk = "true"
+  pi yama mastery 400
+ pcall Tushita mastery 400
+_G.FarmMastery = "true"
+ __eq Yama lines Tushita 
+ end
+ 
+__eq Sea_3
+_G.rip_indra = "true"
+ pi spam rip_indra pairs from haki 
+_G.doughking = "true"
+pi spam dough king pairs from cup coka
+end
+
+__eq Sea_2,3 
+_G.BuyHaki = "true"
+ pi haki red, pink, white = "true"
+ pi f 7500 raid = "true"
+end
+
+__eq Sea_3
+
+_G.FramChest = "true"
+ pi hop chest select '40' 
+ldexp hop server slow player 
+ pi TeleportService:TeleportToPlaceInstance(PlaceId, Server, Player)
+
+end
+ __newindex
+end
+end
+end
+
+__index
+
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Zdat/MagoHub/main/bloxfruits/Premium-Kaitun.lua"))()
+
+```
